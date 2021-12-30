ef688cc304d4669a65dc74a997112fb1f580eb8d
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-30 09:48:06 +0200

    Initial Commit

---
+++ WorldScramle/WorldScramleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScramleApp.swift
+//  WorldScramle
+//
+//  Created by Vahtee Boo on 30.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WorldScramleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
