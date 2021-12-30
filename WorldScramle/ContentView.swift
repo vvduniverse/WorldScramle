c20e6312a63483f776a68716478d386302a4bda8
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-30 12:14:06 +0200

    Lists 01 of 06 part 5

--- WorldScramle/ContentView.swift
+++ WorldScramle/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 0) {
+            
+            List(people, id: \.self) {
+                Text($0)
+            }
+            
+            List {
+                Section("Section 1") {
+                    Text("Static row 1")
+                    Text("Static row 2")
+                }
+                
+                Section("Section 2") {
+                    ForEach(0..<5) {
+                        Text("Dynamic row \($0)")
+                    }
+                }
+                
+                Section("Section 3") {
+                    Text("Static row 3")
+                    Text("Static row 4")
+                }
+            }
+            .listStyle(.sidebar)
+            List(0 ..< 3) { item in
+                Text("Hello, world! \(item)")
+            }
+            List(0 ..< 3) {
+                Text("Hello, world! \($0)")
+            }
+        }
     }
 }
 
